0c948e11cd146130c9b6812b7f57d4a9cb86ce92
Philipp Jardas <philipp@jardas.de>
2018-02-08 10:29:58 +0100

    added server scaffold

---
+++ gui/components/Layout.js
@@ -0,0 +1,15 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+import '../styles.scss';
+
+export default ({ children }) => (
+  <React.Fragment>
+    <Navbar color="primary" dark>
+      <div class="container">
+        <NavbarBrand href="/">Tabi-Disco</NavbarBrand>
+      </div>
+    </Navbar>
+
+    <div>{children}</div>
+  </React.Fragment>
+);
---
+++ gui/pages/index.js
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default () => (
+  <Layout>
+    <div class="container">
+      <p>Hello, World!</p>
+    </div>
+  </Layout>
+);
