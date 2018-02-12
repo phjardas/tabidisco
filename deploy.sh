5cd185894bb9410ac01e562146af3735f0423640
Philipp Jardas <philipp@jardas.de>
2018-02-12 23:18:07 +0100

    added deploy script with systemd on pi

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,11 @@ export GIT_COMMIT_ID
 (cd server && npm install && npm run build)
 (cd gui && npm install && npm run build)
 
-echo "FIXME: Now we need to (re)start the services"
+echo "Installing services"
+sudo cp systemd/* /etc/systemd/system/
+sudo systemctl enable tabidisco-server.service
+sudo systemctl enable tabidisco-gui.service
+
+echo "(Re)starting services"
+sudo systemctl restart tabidisco-server.service
+sudo systemctl restart tabidisco-gui.service
