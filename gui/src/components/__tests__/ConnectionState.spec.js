da63cacea52ce5b1e094ab6d7a82677b234870f2
Philipp Jardas <philipp@jardas.de>
2018-02-16 23:08:27 +0100

    a little bit more testing

--- gui/src/components/__tests__/ConnectionState.spec.js
+++ gui/src/components/__tests__/ConnectionState.spec.js
@@ -16,4 +16,33 @@ describe('ConnectionState', () => {
     expect(alertProps).toHaveProperty('style', { borderRadius: 0 });
     expect(alertProps).toHaveProperty('children', 'Disconnected!');
   });
+
+  it('should render info when connecting', () => {
+    const wrapper = shallow(<ConnectionState state="connecting" />);
+    const alerts = wrapper.find(Alert);
+    expect(alerts).toHaveLength(1);
+    const alert = alerts.first();
+
+    const alertProps = alert.props();
+    expect(alertProps).toHaveProperty('color', 'info');
+    expect(alertProps).toHaveProperty('style', { borderRadius: 0 });
+    expect(alertProps).toHaveProperty('children', 'Connecting...');
+  });
+
+  it('should render warning when reconnecting', () => {
+    const wrapper = shallow(<ConnectionState state="reconnecting" />);
+    const alerts = wrapper.find(Alert);
+    expect(alerts).toHaveLength(1);
+    const alert = alerts.first();
+
+    const alertProps = alert.props();
+    expect(alertProps).toHaveProperty('color', 'warning');
+    expect(alertProps).toHaveProperty('style', { borderRadius: 0 });
+    expect(alertProps).toHaveProperty('children', 'Reconnecting...');
+  });
+
+  it('should render nothing when connected', () => {
+    const wrapper = shallow(<ConnectionState state="connected" />);
+    expect(wrapper.children()).toHaveLength(0);
+  });
 });
